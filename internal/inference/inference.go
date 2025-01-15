358519805412277387e102523337a2c4f8a3deee
icelain <icelain@ice.computer>
2025-01-15 18:30:28 +0530

    start working on inference

---
+++ internal/inference/inference.go
@@ -0,0 +1,9 @@
+package inference
+
+const PROMPT string = `
+
+	You will be provided with a direntry of a git repository. Directories will be marked with [DIR] and files will be marked with [FILE]. 
+
+
+
+`
